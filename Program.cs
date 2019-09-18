11cd57abd915ff8c44ef52d2f6a06f262288f73c
Dylan <Dylan@LAPTOP-HU97QSV9>
2019-09-18 18:54:59 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace deliverableMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your name!");
+            method();
+        }
+        //Custom method
+        private static void method()
+        {
+            string input = Console.ReadLine();
+            Console.WriteLine("Hello" + " " + input + "!");
+            
+        }
+    }
+}
